b1c3acae952366c1a0862ff95d42fec99a502cb3
camryngoins <camryngoins00@gmail.com>
2022-02-21 15:00:48 -0600

    adding if and else if options for different outcomes

--- script.js
+++ script.js
@@ -15,8 +15,6 @@ specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "|",
 
 var selectedOptions;
 
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); 
 
@@ -44,15 +42,34 @@ if (!passwordLength) {
    var passwordCharacters = window.confirm("Would you like to add special characters?");
  };
 
-
-
-
-
-
-
-
-  return "";
+//  if they all equal false, give alert that use must pick an option and to try agaun
+if (!passwordLowerCase && !passwordUpperCase && !passwordNumbers && !passwordCharacters) {
+  selectedOptions = window.alert("You must pick an option. Please try again.");
 }
+// if all are true, proceed
+else if (passwordLowerCase && passwordUpperCase && passwordNumbers && passwordCharacters) {
+  selectedOptions = lowerLetters + upperLetters + numbers + specialCharacters;
+}
+// if all are true except lowercase letters 
+else if (passwordUpperCase && passwordNumbers && passwordCharacters) {
+  selectedOptions = upperLetters + numbers + specialCharacters
+}
+// if all are true except uppercase letters 
+else if (passwordLowerCase && passwordNumbers && passwordCharacters) {
+  selectedOptions = lowerLetters + numbers + specialCharacters;
+}
+// if all are true except numbers
+else if (passwordLowerCase && passwordUpperCase && passwordCharacters) {
+  selectedOptions = lowerLetters + upperLetters + specialCharacters;
+}
+// if all are true except special characters
+else if (passwordLowerCase && passwordUpperCase && passwordNumbers) {
+  selectedOptions = lowerLetters + upperLetters + numbers;
+}
+
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
