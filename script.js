2f6a14f358849b6bde96b842969b5d4bf558925a
camryngoins <camryngoins00@gmail.com>
2022-02-22 13:45:10 -0600

    working on making password generate properly

--- script.js
+++ script.js
@@ -5,26 +5,33 @@
 // 3. Generate the password based off what the user selected.
 // 4. Display the generated password on the page. 
 
-// Assignment code here
-
 // arrays 
 lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "|", "=", "+", "/", "?", "~", "-", "<", ">"];
 
+// variables
+var passwordLowerCase;
+var passwordUpperCase;
+var passwordNumbers;
+var passwordCharacters;
+var passwordLength;
+
 var selectedOptions;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); 
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // generate password
 function generatePassword() {
   //ask for the character length
- var passwordLength =  window.prompt("Pick a number length between 8 and 128 characters.")
+ passwordLength =  window.prompt("Pick a number length between 8 and 128 characters.")
   //convert the string to a number 
  passwordLength = parseInt(passwordLength)
- console.log(passwordLength);
   // if user chooses to opt out of choosing a password length, give them an alert to let them know they have to choose.
 if (!passwordLength) {
   window.alert("Please select a password length.") 
@@ -34,12 +41,12 @@ if (!passwordLength) {
  else if (passwordLength >= 129 || passwordLength <= 7 ) {
    window.alert("Please pick a number between 8 and 128 characters!");
    return;
- }
- else {
-   var passwordLowerCase = window.confirm("Would you like to add lowercase letters?");
-   var passwordUpperCase = window.confirm("Would you like to add uppercase letters?");
-   var passwordNumbers = window.confirm("Would you like to add numbers?")
-   var passwordCharacters = window.confirm("Would you like to add special characters?");
+
+ } else {
+  passwordLowerCase = window.confirm("Would you like to add lowercase letters?");
+  passwordUpperCase = window.confirm("Would you like to add uppercase letters?");
+  passwordNumbers = window.confirm("Would you like to add numbers?")
+  passwordCharacters = window.confirm("Would you like to add special characters?");
  };
 
 //  if they all equal false, give alert that use must pick an option and to try agaun
@@ -52,7 +59,7 @@ else if (passwordLowerCase && passwordUpperCase && passwordNumbers && passwordCh
 }
 // if all are true except lowercase letters 
 else if (passwordUpperCase && passwordNumbers && passwordCharacters) {
-  selectedOptions = upperLetters + numbers + specialCharacters
+  selectedOptions = upperLetters + numbers + specialCharacters;
 }
 // if all are true except uppercase letters 
 else if (passwordLowerCase && passwordNumbers && passwordCharacters) {
@@ -65,9 +72,16 @@ else if (passwordLowerCase && passwordUpperCase && passwordCharacters) {
 // if all are true except special characters
 else if (passwordLowerCase && passwordUpperCase && passwordNumbers) {
   selectedOptions = lowerLetters + upperLetters + numbers;
-}
+} 
 
-}
+var myPassword = "";
+
+for (var i = 0; i < passwordLength; i++) {
+  var outcome = selectedOptions[Math.floor(Math.random() * selectedOptions.length)];
+  myPassword += selectedOptions
+} 
+return myPassword;
+};
 
 
 
@@ -80,5 +94,3 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
